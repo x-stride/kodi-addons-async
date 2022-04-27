21fa2d3b1c9dfb2a33b26230a7d6d26e2a1617bf
morten.karlsen <morten.karlsen@omniaccess.com>
2022-04-28 00:22:23 +0200

    fix: generalizse get_event_loop concept

--- service.aiohttp.service/service.py
+++ service.aiohttp.service/service.py
@@ -1,55 +1,66 @@
 import asyncio
+
 import xbmc
-from aiohttp import web
+from aiohttp import web, ClientSession
+
+from lib.event_loop import get_running_loop
 
 
-async def hello(request):
-    xbmc.log('AIOHTTP Application Request: !!!', xbmc.LOGDEBUG)
-    return web.Response(text="Hello, world\n")
+class Service:
+    def __init__(self, host: str = 'localhost', port: int = 8080):
+        self.host = host
+        self.port = port
 
+    @staticmethod
+    async def hello(request):
+        xbmc.log('AIOHTTP Application Request: !!!', xbmc.LOGDEBUG)
 
-async def serve():
-    xbmc.log('AIOHTTP Application Starting', xbmc.LOGDEBUG)
-    app = web.Application()
-    xbmc.log('AIOHTTP Adding route and handler "hello"', xbmc.LOGDEBUG)
-    app.add_routes([web.get('/', hello)])
-    xbmc.log('AIOHTTP Creating runner', xbmc.LOGDEBUG)
-    runner = web.AppRunner(app)
-    xbmc.log('AIOHTTP runner setup', xbmc.LOGDEBUG)
-    await runner.setup()
-    xbmc.log('AIOHTTP Creating site', xbmc.LOGDEBUG)
-    site = web.TCPSite(runner, '192.168.0.175', 1080)
-    xbmc.log('AIOHTTP Starting site', xbmc.LOGDEBUG)
-    await site.start()
-    try:
-        while True:
-            await asyncio.sleep(4)
-    except Exception:
-        xbmc.log('AIOHTTP Shutting down', xbmc.LOGDEBUG)
-        await runner.cleanup()
+        async with ClientSession() as session:
+            xbmc.log(f'AIOHTTP TEST running...', xbmc.LOGDEBUG)
+            async with session.get('http://httpbin.org/get') as resp:
+                await resp.text()
 
+        text = f'Hello there! Wanna run a test? Ups ran it already...\nhttp://httpbin.org/get.resp={resp}'
+        return web.Response(
+            text=text)
 
-def event_loop():
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
+    async def serve(self):
+        xbmc.log(f'AIOHTTP Application Starting, http://{self.host}:{self.port}', xbmc.LOGDEBUG)
+        app = web.Application()
+
+        xbmc.log(f'AIOHTTP Adding route and handler "hello", http://{self.host}:{self.port}', xbmc.LOGDEBUG)
+        app.add_routes([web.get('/', self.hello)])
+
+        xbmc.log(f'AIOHTTP Creating runner, http://{self.host}:{self.port}', xbmc.LOGDEBUG)
+        runner = web.AppRunner(app)
+
+        xbmc.log(f'AIOHTTP Runner setup, http://{self.host}:{self.port}', xbmc.LOGDEBUG)
+        await runner.setup()
+
+        xbmc.log(f'AIOHTTP Creating site, http://{self.host}:{self.port}', xbmc.LOGDEBUG)
+        site = web.TCPSite(runner, self.host, self.port)
+
+        xbmc.log(f'AIOHTTP Starting site, http://{self.host}:{self.port}', xbmc.LOGDEBUG)
+        await site.start()
+
         try:
-            loop = asyncio.new_event_loop()
-        except RuntimeError:
-            loop = asyncio.get_running_loop()
-    return loop
+            while True:
+                await asyncio.sleep(4)
+        except NameError:  # help, I'm gone!
+            xbmc.log(f'AIOHTTP Shutting down, http://{self.host}:{self.port}', xbmc.LOGDEBUG)
+            await runner.cleanup()
 
 
 class Monitor(xbmc.Monitor):
-    def run(self):
-        xbmc.log('AIOHTTP getting event-loop', xbmc.LOGDEBUG)
-        loop = event_loop()
-        loop.create_task(serve())
+    def run(self, host: str = 'localhost', port: int = 1081):
+        xbmc.log(f'AIOHTTP getting event-loop, http://{host}:{port}', xbmc.LOGDEBUG)
+        loop = get_running_loop()
+        service = Service(host=host, port=port)
+        loop.create_task(service.serve())
         while not self.abortRequested():
             if self.waitForAbort(1):
                 break
 
 
-if __name__ == '__main__':
-    xbmc.sleep(1000)
-    Monitor().run()
+xbmc.sleep(2500)
+Monitor().run()
